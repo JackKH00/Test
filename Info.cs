375bf2ff9d9ba8b0b053f3ba2fb3e80f625f9b66
JackKH00 <54737634+JackKH00@users.noreply.github.com>
2019-08-31 11:31:29 +0300

    Update Info.cs

--- Info.cs
+++ Info.cs
@@ -17,4 +17,8 @@ class Program
     {
         Console.WriteLine("Тут была Ксюха!");
     }
+    static void TheTrueMethod()
+    {
+        Console.Writeline("Therefore thus saith the Lord, Behold, I will bring evil upon them, which they shall not be able to escape; and though they shall cry unto me, I will not hearken unto them.");
+    }
 }
